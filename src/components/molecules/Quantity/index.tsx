9965b9f3a40c55e662a2446e33e559e7d8f0ea76
Caroline Nogueira <carol.ngr8@gmail.com>
2024-08-13 23:51:28 -0300

    refactor: component quantity

--- src/components/molecules/Quantity/index.tsx
+++ src/components/molecules/Quantity/index.tsx
@@ -2,26 +2,22 @@ import { Minus, Plus } from "phosphor-react-native";
 import { Add, Container, Decrement, QuantityNumber } from "./styles";
 import { useState } from "react";
 
-export const Quantity = () => {
-  const [quantity, setQuantity] = useState(0);
+type Props = {
+  increment: () => void;
+  decrement: () => void;
+  quantity: number;
+};
 
-  const handleIncrement = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const handleDecrement = () => {
-    if (quantity > 0) setQuantity(quantity - 1);
-  };
-
+export const Quantity = ({ increment, decrement, quantity }: Props) => {
   return (
     <Container>
-      <Decrement onPress={handleDecrement}>
+      <Decrement onPress={() => decrement()}>
         <Minus size={14} />
       </Decrement>
 
       <QuantityNumber>{quantity}</QuantityNumber>
 
-      <Add onPress={handleIncrement}>
+      <Add onPress={() => increment()}>
         <Plus size={14} />
       </Add>
     </Container>
