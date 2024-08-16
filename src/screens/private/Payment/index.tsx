1a577fac67b42748c3557c1394f13b73dff30ee0
Caroline Nogueira <carol.ngr8@gmail.com>
2024-08-16 19:55:29 -0300

    refactor: Page payment edit cart and payment options

--- src/screens/private/Payment/index.tsx
+++ src/screens/private/Payment/index.tsx
@@ -3,24 +3,30 @@ import { Headers } from "@components/templates/Headers";
 import {
   Address,
   Container,
-  PaymentCart,
   PaymentOptions,
   FiveInputsRow,
   SmallInput,
+  Option,
+  CredCart,
+  TitleName,
+  Form,
 } from "./styles";
 import { CaretRight, Plus, Wallet } from "phosphor-react-native";
 import { Box } from "@components/atomos/Box";
 import { PaymentInfoArea } from "@components/molecules/PaymentInfoArea";
 import { Button } from "@components/molecules/Button";
 import { Input } from "@components/molecules/Input";
+import { useState } from "react";
 
 export function Payment() {
+  const [isShowForm, setShowForm] = useState<boolean>(false);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Headers.Simple title="Carrinho" showBackButton />
+      <Headers.Simple title="Pagamento" showBackButton />
       <Container>
-        <Box>
-          <Text>Jóse da Silva</Text>
+        <CredCart>
+          <TitleName>Jóse da Silva</TitleName>
           <View>
             <Text>VALID THRU</Text>
             <TextInput
@@ -30,36 +36,59 @@ export function Payment() {
             />
           </View>
           <FiveInputsRow>
-            {[...Array(5)].map((_, index) => (
+            {[...Array(4)].map((_, index) => (
               <SmallInput key={index} keyboardType="numeric" maxLength={1} />
             ))}
+            <SmallInput keyboardType="numeric" maxLength={1} />
           </FiveInputsRow>
-        </Box>
+        </CredCart>
 
         <PaymentOptions>
-          <Plus size={32} />
-          <Text>Cadastrar cartão</Text>
-          <Wallet size={32} />
-          <Text>Trocar Cartão</Text>
+          <Option>
+            <Button.Icon
+              name="plus"
+              weight="fill"
+              size={24}
+              onPress={() => setShowForm(true)}
+            />
+            <Text>Cadastrar cartão</Text>
+          </Option>
+          <Option>
+            <Button.Icon name="wallet" weight="fill" size={24} />
+            <Text>Trocar Cartão</Text>
+          </Option>
         </PaymentOptions>
 
-        <Box>
-          <Address>
+        {isShowForm ? (
+          <Form>
+            <Input.InputText placeholder="Nome do titular" />
+            <Input.InputText placeholder="Número do cartão" />
             <View>
-              <Text>Endereço</Text>
-              <Text>Rua são martins, 79</Text>
-              <Text>CENTRO, João Pessoa / PB</Text>
+              <Input.InputText placeholder="Validade" />
+              <Input.InputText placeholder="CVV" />
             </View>
-            <CaretRight size={32} />
-          </Address>
-        </Box>
+          </Form>
+        ) : (
+          <>
+            <Box>
+              <Address>
+                <View>
+                  <Text>Endereço</Text>
+                  <Text>Rua são martins, 79</Text>
+                  <Text>CENTRO, João Pessoa / PB</Text>
+                </View>
+                <CaretRight size={32} />
+              </Address>
+            </Box>
 
-        <PaymentInfoArea />
-        <Button.Primary
-          title="Confirmar pagamento"
-          type="SECONDARY"
-          onPress={() => console.log("teste")}
-        />
+            <PaymentInfoArea subtotal={} valorFrete={29.9} />
+            <Button.Primary
+              title="Confirmar pagamento"
+              type="SECONDARY"
+              onPress={() => console.log("teste")}
+            />
+          </>
+        )}
       </Container>
     </SafeAreaView>
   );
--- src/screens/private/Payment/styles.ts
+++ src/screens/private/Payment/styles.ts
@@ -1,21 +1,43 @@
-import styled from "styled-components/native";
+import { Box } from "@components/atomos/Box";
+import styled, { css } from "styled-components/native";
+import { color } from "styled-system";
 
 export const Container = styled.View`
   padding: 24px;
   gap: 20px;
 `;
 
+export const CredCart = styled(Box)`
+  width: 100%;
+  padding: 37px 24px;
+  background-color: #3f3f3f;
+`;
+
+export const TitleName = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.SM};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.WHITE};
+  `}
+  padding-bottom: 30px;
+`;
+
 export const PaymentOptions = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
 `;
 
-export const PaymentCart = styled.View`
+export const Option = styled.View`
   flex-direction: row;
-  justify-content: space-between;
   align-items: center;
-  gap: 4px;
+  gap: 6px;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.SM};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
 `;
 
 export const FiveInputsRow = styled.View`
@@ -24,7 +46,7 @@ export const FiveInputsRow = styled.View`
 `;
 
 export const SmallInput = styled.TextInput`
-  border-bottom-width: 1px;
+  border-width: 1px;
   border-bottom-color: #ddd;
   width: 50px;
   padding: 5px;
@@ -35,4 +57,8 @@ export const Address = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+`;
+
+export const Form = styled.View`
+  gap: 10px;
 `;
