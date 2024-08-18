5bff768e77f20a25089ca94a39b1446eece0e1c5
Caroline Nogueira <carol.ngr8@gmail.com>
2024-08-18 17:57:36 -0300

    feat: Add onpress component infoblock

--- src/screens/private/ProfileOrderHistory/index.tsx
+++ src/screens/private/ProfileOrderHistory/index.tsx
@@ -1,16 +1,39 @@
-import { SafeAreaView } from "react-native";
+import { FlatList, SafeAreaView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/botton-tabs.routes";
+
 import { Headers } from "@components/templates/Headers";
 import { Content } from "@components/atomos/Content";
 import { InfoBlock } from "@components/molecules/InfoBlock";
 
+const data = [
+  { id: "1", title: "Item 1", description: "Descrição do Item 1" },
+  { id: "2", title: "Item 2", description: "Descrição do Item 2" },
+  { id: "3", title: "Item 3", description: "Descrição do Item 3" },
+  { id: "4", title: "Item 3", description: "Descrição do Item 4" },
+  { id: "5", title: "Item 3", description: "Descrição do Item 5" },
+  { id: "6", title: "Item 3", description: "Descrição do Item 6" },
+  { id: "7", title: "Item 3", description: "Descrição do Item 7" },
+];
+
 export function ProfileOrderHistory() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Headers.Simple title="Meus Pedidos" showBackButton />
       <Content>
-        <InfoBlock
-          title="Pedido#1"
-          description="Valor: 299,99 {/n} Status: Pedido Confirmado"
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <InfoBlock
+              title={item.title}
+              description={item.description}
+              onPress={() => navigation.navigate("orderview")}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{ gap: 28 }}
         />
       </Content>
     </SafeAreaView>
