cc1a60260a6aef19b13da6997e46b5f55e049cf4
Caroline Nogueira <carol.ngr8@gmail.com>
2024-08-18 10:00:10 -0300

    feat: Add new icons

--- src/components/atomos/Icons/index.tsx
+++ src/components/atomos/Icons/index.tsx
@@ -1,10 +1,19 @@
 import { View } from "react-native";
-import { IconWeight, Minus, Plus, Wallet } from "phosphor-react-native";
+import {
+  CaretRight,
+  CheckSquare,
+  IconWeight,
+  Minus,
+  Plus,
+  Wallet,
+} from "phosphor-react-native";
 
 const glyphMap = {
   plus: Plus,
   minus: Minus,
   wallet: Wallet,
+  caretRight: CaretRight,
+  checkSquare: CheckSquare,
 };
 
 type IconName = keyof typeof glyphMap;
